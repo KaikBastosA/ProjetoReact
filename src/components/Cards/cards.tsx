3e63d17ead3140ae8a51ce0146b35fd6311974f4
Kaik Bastos <kaikbastos@id.uff.br>
2025-02-23 10:53:50 -0300

    (feat): Adicionado e estilizado o Body do card

--- src/components/Cards/cards.tsx
+++ src/components/Cards/cards.tsx
@@ -1,9 +1,11 @@
 import Header from './Header/header'
 import styles from './style.module.css'
+import Body from './Body/body'
 export default function cards() {
     return (
         <section className={styles.card}>
             <Header/>
+            <Body/>
         </section>
     )
 }
