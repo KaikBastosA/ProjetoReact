92c105b6c304e8d2d9029f884fd17e02db798704
Kaik Bastos <kaikbastos@id.uff.br>
2025-02-23 14:17:24 -0300

    (refact): Refatorado o texto de foto para Photo, mantendo o padrão no codigo

--- src/components/Perfil/perfil.tsx
+++ src/components/Perfil/perfil.tsx
@@ -1,10 +1,10 @@
 import styles from './style.module.css'
-import Foto from '../../assets/fotoEu.jpg'
+import Photo from '../../assets/fotoEu.jpg'
 
 export default function perfil() {
     return (
         <div className={styles.profile}>
-            <img className={styles.photo} src={Foto} alt="Foto de perfil" />
+            <img className={styles.photo} src={Photo} alt="Foto de perfil" />
             <div className={styles.info}>
                 <h1>Kaik Bastos</h1>
                 <h2>Desenvolvedor Front-End</h2>
