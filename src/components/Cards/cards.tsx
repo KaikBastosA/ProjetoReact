a6cc4dd52e100135129f47506c7220ff80457b58
Kaik Bastos <kaikbastos@id.uff.br>
2025-02-23 10:35:04 -0300

    (feat): Estilizado o header do card

--- src/components/Cards/cards.tsx
+++ src/components/Cards/cards.tsx
@@ -1,8 +1,8 @@
 import Header from './Header/header'
-
+import styles from './style.module.css'
 export default function cards() {
     return (
-        <section className="card">
+        <section className={styles.card}>
             <Header/>
         </section>
     )
